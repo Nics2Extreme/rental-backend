7bff8a7c7d691ac972285fc240e9a3c610ac85d2
Nics2Extreme <J.Nics016@gmail.com>
2023-04-16 22:40:25 +0800

    updated backend for user registration and authentication

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,6 @@
 const allowedOrigins = [
-    'https://www.yoursite.com',
-    'http://127.0.0.1:5500',
-    'http://localhost:3500',
+    'https://marlynshouserental.com',
+    'http://127.0.0.1:5173',
     'http://localhost:3000'
 ];
 
